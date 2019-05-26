5112c8a4ade38386ad2d37d68ae07ab6be20292a
Michel Bolzon <michel.bolzon123@Gmail.com>
2019-05-26 19:24:02 -0300

    teste mysql

---
+++ teste/teste.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "127.0.0.1";
+$database = "ot";
+$username = "michel";
+$password = '123456!@#$$Michel';
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
