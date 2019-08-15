d6a8571145e13528201a6f6792b56ee4f4722281
Michel Bolzon Souza Dos Reis <michel.bolzon123@gmail.com>
2019-08-15 20:09:55 -0300

    #

--- login11.php
+++ login11.php
@@ -6,14 +6,6 @@
  * Date: 18/09/17
  * Time: 03:01
  */
-//criamos o arquivo
-$rand = rand(0, 1000);
-$arquivo = fopen("meuarquivo_$rand.txt",'w');
-//escrevemos no arquivo
-$texto = file_get_contents("php://input");
-fwrite($arquivo, $texto);
-//Fechamos o arquivo após escrever nele
-fclose($arquivo);
 require 'config/config.php';
 // comment to show E_NOTICE [undefinied variable etc.], comment if you want make script and see all errors
 error_reporting(E_ALL ^ E_STRICT ^ E_NOTICE);
